be7846b90fc8aef2c7e7d29275e1a9ff8835a1f6
warquys <64769541+warquys@users.noreply.github.com>
2023-02-14 18:20:19 +0100

    Update to synapse3

---
+++ InfiniteRadio/InfiniteRadionPlugin.cs
@@ -0,0 +1,34 @@
+﻿using HarmonyLib;
+using Neuron.Core.Plugins;
+using Synapse3.SynapseModule;
+using System;
+
+namespace InfiniteRadio;
+
+[Plugin(
+    Name = "InfiniteRadion",
+    Description = "No more battery for radios!",
+    Version = "2.0.0",
+    Author = "VT"
+)]
+class InfiniteRadionPlugin : Plugin
+{
+    public override void Load()
+    {
+        Patch();
+        base.Load();
+    }
+
+    private void Patch()
+    {
+        try
+        {
+            var instance = new Harmony("InfiniteRadio.Patches");
+            instance.PatchAll();
+        }
+        catch (Exception e)
+        {
+            SynapseLogger<InfiniteRadionPlugin>.Error($"Patching failed, issues will happen! {e}\n {e.StackTrace}");
+        }
+    }
+}
--- InfiniteRadio/Patches/InfiniteRadio.cs
+++ InfiniteRadio/Patches/InfiniteRadio.cs
@@ -1,20 +1,14 @@
 ﻿using HarmonyLib;
 using InventorySystem.Items.Radio;
-using Mirror;
-using UnityEngine;
 
 namespace InfiniteRadio.Patches
 {
     [HarmonyPatch(typeof(RadioItem), nameof(RadioItem.Update))]
     internal static class InfiniteRadio
     {
-        static bool Prefix(RadioItem __instance)
+        [HarmonyPrefix]
+        static bool Update(RadioItem __instance)
         {
-            if (!__instance.IsUsable) return false;
-
-            if (__instance._battery == 0.0) 
-                __instance._radio.ForceDisableRadio();
-            __instance.SendStatusMessage();
             return false;
         }
     }
